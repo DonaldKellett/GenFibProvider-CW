9fc4330ba1b652f4b69a80ca4cbe6e1fb5fa9e32
Donald Sebastian Leung <donaldsebleung@gmail.com>
2021-05-11 04:43:33 +0000

    Structure dummy test

--- GenFibProvider.Tests.ps1
+++ GenFibProvider.Tests.ps1
@@ -1,9 +1,21 @@
-Import-Module SHiPS
-Move-Item GenFibProvider.ps1 -Destination GenFibProvider.psm1 -Force
-Import-Module ./GenFibProvider.psm1
-New-PSDrive -Name GF -Root GenFibProvider#GenFib -PSProvider SHiPS
-Get-PSDrive
-Remove-PSDrive -Name GF
-Remove-Module GenFibProvider
-Move-Item GenFibProvider.psm1 -Destination GenFibProvider.ps1 -Force
-Remove-Module SHiPS
+BeforeAll {
+  Write-Host "Running BeforeAll"
+  Import-Module SHiPS
+  Move-Item GenFibProvider.ps1 -Destination GenFibProvider.psm1 -Force
+  Import-Module ./GenFibProvider.psm1
+  New-PSDrive -Name GF -Root GenFibProvider#GenFib -PSProvider SHiPS
+}
+
+Describe 'Dummy' {
+  It '1 should equal 1' {
+    1 | Should -Be 1
+  }
+}
+
+AfterAll {
+  Write-Host "Running AfterAll"
+  Remove-PSDrive -Name GF
+  Remove-Module GenFibProvider
+  Move-Item GenFibProvider.psm1 -Destination GenFibProvider.ps1 -Force
+  Remove-Module SHiPS
+}
