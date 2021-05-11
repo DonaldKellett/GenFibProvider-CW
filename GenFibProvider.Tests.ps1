5f1e6d7e664b1a3fa046a516f5f394f7e7ee95e2
Donald Sebastian Leung <donaldsebleung@gmail.com>
2021-05-11 06:19:53 +0000

    Add simple fixed test

--- GenFibProvider.Tests.ps1
+++ GenFibProvider.Tests.ps1
@@ -14,6 +14,15 @@ Describe 'GenFibProvider' {
     Get-Content GF:\Count | Should -Be '10'
     Get-Content GF:\Sequence | Should -Be '0,1,1,2,3,5,8,13,21,34'
   }
+  It 'should allow Set-Content on Order, Start and Count' {
+    Set-Content GF:\Order -Value 3
+    Set-Content GF:\Start -Value 15
+    Set-Content GF:\Count -Value 20
+    Get-Content GF:\Order | Should -Be '3'
+    Get-Content GF:\Start | Should -Be '15'
+    Get-Content GF:\Count | Should -Be '20'
+    Get-Content GF:\Sequence | Should -Be '1705,3136,5768,10609,19513,35890,66012,121415,223317,410744,755476,1389537,2555757,4700770,8646064,15902591,29249425,53798080,98950096,181997601'
+  }
   AfterEach {
     Remove-PSDrive -Name GF
   }
